1c5a18b2050461eaeac42abbc68af9f178a58c7d
francesco <p21fthomas@fairfieldprep.org>
2020-10-23 12:20:47 -0400

    lab_1-1

---
+++ lab_1-1.py
@@ -0,0 +1,26 @@
+#int(a) 
+# name is not defined
+
+
+#int('a') 
+# Value error
+
+
+#'a' + 2
+#  TypeError
+
+
+#import date 
+# ModuleNotFoundError
+
+
+#print("I'm a happy camper!)
+#syntaxError
+
+#x = True 
+#x = x + 'abc'
+#TypeError
+
+
+x = str('abc')
+x = int(x)
